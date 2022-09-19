a1ce029688a86e7b5572a7a5b9266e33dead9cc3
Galina Shchepotina <galsepo@gmail.com>
2022-09-19 11:12:16 -0700

    adding cart context and shopping icon

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Navigation from './routes/navigation/navigation.component';
 
 import Authentication from './routes/authentication/authentication.component'
 
-const Shop = () => {
-  return <h1>I an shop page</h1>;
-};
+import Shop from "./routes/shop/shop.component"
 
 const App = () => {
   return (
---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import './cart-icon.styles.scss';
+
+import { CartContext } from '../../contexts/card.context'
+
+const CartIcon = () => {
+    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+
+    const toogleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+    return (
+        <div className='cart-icon-container' onClick={toogleIsCartOpen}>
+            <ShoppingIcon className='shopping-icon' />
+            <span className='item-count'>1</span>
+        </div>
+
+    );
+
+};
+
+export default CartIcon;
