209ecc760ce0687398d994ea6357feac7311d551
Galina Shchepotina <galsepo@gmail.com>
2022-09-25 10:07:20 -0700

    add cart and user redusers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,6 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
+
+import { createAction } from '../utils/reducer/reduser.utils';
 
 import {
   onAuthStateChangedListener,
@@ -10,15 +12,50 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReduser = (state, action) => {
+  console.log('userReduser dispatched');
+  console.log({ action });
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      }
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+
+}
+const INITIAL_STATE = {
+  currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  console.log("UserProvider")
+  const [state, dispatch] = useReducer(userReduser, INITIAL_STATE);
+  const { currentUser } = state;
+  console.log({ currentUser });
+
+  const setCurrentUser = (user) => {
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
+  }
+
+  // const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
+      console.log("ChangedListener")
       if (user) {
         createUserDocumentFromAuth(user);
       }
+      console.log("setCurrentUser in useEffect")
       setCurrentUser(user);
     });
 
--- src/index.js
+++ src/index.js
@@ -12,17 +12,17 @@ import './index.scss';
 
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 rootElement.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <UserProvider>
+      <CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </CategoriesProvider>
+    </UserProvider>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
